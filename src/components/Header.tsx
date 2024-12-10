5fb94f2c04b0dbe3d887e96284adec2df312d1ce
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-12-11 01:45:55 +0200

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='sticky top-0 z-10 border  rounded p-2'>
-      <div className='flex items-center space-x-4 font-bold text-md text-black uppercase'>
+      <div className='flex items-center space-x-4 font-bold text-md text-slate-600 uppercase'>
         <div className='w-1/6 '>subject</div>
         <div className='w-1/6 '>priority</div>
         <div className='w-1/6'>status</div>
--- src/components/TicketRow.tsx
+++ src/components/TicketRow.tsx
@@ -15,26 +15,26 @@ const TicketRow: React.FC<TicketRowProps> = React.memo(({ ticket }) => {
   // styling based on value
   const priorityColor =
     priority.toLowerCase() === 'high'
-      ? 'text-red-600'
+      ? 'text-red-500'
       : priority.toLowerCase() === 'medium'
-      ? 'text-yellow-600'
-      : 'text-green-600'
+      ? 'text-yellow-700'
+      : 'text-green-700'
   const statusColor =
     status.toLowerCase() === 'open'
-      ? 'text-red-600'
+      ? 'text-red-500'
       : status === 'In Progress'
-      ? 'text-yellow-600'
-      : 'text-green-600'
+      ? 'text-yellow-700'
+      : 'text-green-700'
 
   return (
     <div className='shadow-sm rounded-md border border-gray-300 p-3 bg-gray-50 mb-2'>
       <div className='flex items-center space-x-4'>
-        <div className='w-1/6 font-bold text-sm text-gray-800 truncate'>
+        <div className='w-1/6 font-bold text-md text-gray-800 truncate'>
           {subject}
         </div>
-        <div className={`w-1/6 text-sm ${priorityColor}`}>{priority}</div>
-        <div className={`w-1/6 text-sm ${statusColor}`}>{status}</div>
-        <div className='w-1/2 text-sm text-gray-600 truncate'>
+        <div className={`w-1/6 text-md ${priorityColor}`}>{priority}</div>
+        <div className={`w-1/6 text-md ${statusColor}`}>{status}</div>
+        <div className='w-1/2 text-md text-gray-600 truncate'>
           {description}
         </div>
       </div>
