6d03e267b8417b75d262e861c2b580fc2fea97eb
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-12-11 01:27:46 +0200

    fix error

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import VirtualizedTicketList from './components/VirtualizedTicketList';
-const App = () => {
-    const ticketsNumber = 10000;
-    return (_jsxs("div", { className: 'min-h-screen bg-black flex flex-col items-center justify-center', children: [_jsx("h1", { className: 'text-xl font-semibold text-gray-100 ', children: "Ticket List" }), _jsxs("h3", { className: 'text-gray-200 m-1', children: ["Total No of tickets:", _jsxs("span", { className: 'font-bold ml-1 text-green-500', children: [" ", ticketsNumber] })] }), _jsx("div", { className: 'w-full xl:w-4/5 mt-2', children: _jsx(VirtualizedTicketList, { ticketsNumber: ticketsNumber }) })] }));
-};
-export default App;
